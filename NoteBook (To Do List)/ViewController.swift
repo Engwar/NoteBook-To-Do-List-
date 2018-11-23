733878384ecbd934ec071d16e34f236ae6b9474d
Igor Shelginskiy <engwar@macbook-pro-igor.local>
2018-11-23 20:33:25 +0300

    Initial Commit

---
+++ NoteBook (To Do List)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoteBook (To Do List)
+//
+//  Created by Igor Shelginskiy on 11/23/18.
+//  Copyright © 2018 Igor Shelginskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
